d7867ba0755534016d9056d0629bfffbf1e5304b
Vincenzo Alberto Marrari <vincenzoalbertomarrari@gmail.com>
2026-06-22 16:20:53 +0200

    config: change dev server port to 5180

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   base: './',
+  server: {
+    port: 5180
+  },
   test: {
     environment: 'node',
     include: ['tests/**/*.test.ts'],
